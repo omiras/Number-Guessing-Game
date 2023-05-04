8523b134c6cc410467fa3c78e98e0395fb4938d6
Oscar Miras <oscar.miras@gmail.com>
2023-05-04 10:00:26 +0200

    updated

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 // Good luck!
 
+let estoyJugando = true;
+let numeroCorrecto = Math.floor(Math.random() * 100);
+let numIntentos = 10;
+
 document.querySelector("form").addEventListener("click", (e) => {
   e.preventDefault(); // Impedimos que el formulario haga un petición GET
 });

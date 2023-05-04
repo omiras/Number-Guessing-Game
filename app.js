65dca9813aa5fec5d17df5a7df9422420db8281d
Oscar Miras <oscar.miras@gmail.com>
2023-05-04 10:12:21 +0200

    form

--- app.js
+++ app.js
@@ -4,6 +4,10 @@ let estoyJugando = true;
 let numeroCorrecto = Math.floor(Math.random() * 100);
 let numIntentos = 10;
 
-document.querySelector("form").addEventListener("click", (e) => {
+// Actualizar el innerHTML del nodo adecuado con el número de intentos iniciales
+
+document.querySelector("form").addEventListener("submit", (e) => {
   e.preventDefault(); // Impedimos que el formulario haga un petición GET
+
+  // Comprobar si el número que ha puesto el usuario es mayor o menor que el numeroCorrecto. Tomar decisiones
 });

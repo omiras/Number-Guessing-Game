baa1d1791e734719802d04f7ad63b0623669f82a
Oscar Miras <oscar.miras@gmail.com>
2025-04-02 09:55:41 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,15 +1,1 @@
-// Good luck!
-
-// Variables de estado iniciales. Puede que falta alguna...
-
-let estoyJugando = true;
-let numeroCorrecto = Math.floor(Math.random() * 100);
-let numIntentos = 10;
-
-// Actualizar el innerHTML del nodo adecuado con el número de intentos iniciales
-
-document.querySelector("form").addEventListener("submit", (e) => {
-  e.preventDefault(); // Impedimos que el formulario haga un petición GET
-
-  // Comprobar si el número que ha puesto el usuario es mayor o menor que el numeroCorrecto. Tomar decisiones
-});
+// ¡Buena suerte!
